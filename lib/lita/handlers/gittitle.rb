6d46b5e1f07dfe4b0477af3bb5014a0f6b01d20d
nyomoko <nyomoko@gmail.com>
2017-01-23 17:17:19 +0900

    設定ファイルの値を使う用にした

--- lib/lita/handlers/gittitle.rb
+++ lib/lita/handlers/gittitle.rb
@@ -1,8 +1,21 @@
 module Lita
   module Handlers
     class Gittitle < Handler
+      config :web_endpoint
+      config :api_endpoint
+      config :token
       # insert handler code here
-
+      route(/git.pepabo.com\/(.+)\/issues\/([0-9]+)/i,
+             :issue,help:{"GHEのissueのURL" => "タイトルを返す"})
+      route(/(.+\/.+)#([0-9]+)/i,:issue) 
+      def issue(response)
+        octocat = Octokit::Client.new(
+	          :access_token =>config.token,
+		  :web_endpoint=> config.web_endpoint, 
+		  :api_endpoint =>config.api_endpoint)
+        issue =  octocat.issue(response.matches[0][0],response.matches[0][1]);
+	response.reply("https://git.pepabo.com/" + response.matches[0][0] + "/issues/" + response.matches[0][1] + "\n" + issue.title)
+      end
       Lita.register_handler(self)
     end
   end
