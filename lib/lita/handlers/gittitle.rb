c0f10dca5a35bd6cf750f6924bef997a72ad0110
nyomoko <nyomoko@gmail.com>
2017-01-25 09:49:16 +0900

    とりあえずissueのタイトル取得ができる様になった

--- lib/lita/handlers/gittitle.rb
+++ lib/lita/handlers/gittitle.rb
@@ -1,20 +1,28 @@
 module Lita
   module Handlers
     class Gittitle < Handler
-      config :web_endpoint
+		  on :loaded, :define_github_url
+      config(:web_endpoint,type: String, required: true)
       config :api_endpoint
       config :token
       # insert handler code here
-      route(/git.pepabo.com\/(.+)\/issues\/([0-9]+)/i,
-             :issue,help:{"GHEのissueのURL" => "タイトルを返す"})
+      #route(/git.pepabo.com\/(.+)\/issues\/([0-9]+)/i,:issue,help:{"GHEのissueのURL" => "タイトルを返す"})
       route(/(.+\/.+)#([0-9]+)/i,:issue) 
+			def define_github_url(payload)
+        regexp = config.web_endpoint + "\/(.+)\/issues\/([0-9]+)"
+        self.class.route(/#{regexp}/i,:issue,help:{"GHEのissueのURL" => "タイトルを返す"})
+			end
+			def octokit()
+				Octokit.reset!
+				@@octokit = Octokit::Client.new(
+	      :access_token =>config.token,
+        :web_endpoint=> config.web_endpoint, 
+        :api_endpoint =>config.api_endpoint)
+		  end 	
       def issue(response)
-        octocat = Octokit::Client.new(
-	          :access_token =>config.token,
-		  :web_endpoint=> config.web_endpoint, 
-		  :api_endpoint =>config.api_endpoint)
-        issue =  octocat.issue(response.matches[0][0],response.matches[0][1]);
-	response.reply("https://git.pepabo.com/" + response.matches[0][0] + "/issues/" + response.matches[0][1] + "\n" + issue.title)
+				octokit()
+        issue =  @@octokit.issue(response.matches[0][0],response.matches[0][1])
+	      response.reply("https://git.pepabo.com/" + response.matches[0][0] + "/issues/" + response.matches[0][1] + "\n" + issue.title)
       end
       Lita.register_handler(self)
     end
