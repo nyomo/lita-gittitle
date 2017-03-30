c1b202663d98f70b587491c18046175e1a090f56
nyomoko <tomoko.ymda@pepabo.com>
2017-03-30 19:33:45 +0900

    プルリクエストのタイトルを取得

--- lib/lita/handlers/gittitle.rb
+++ lib/lita/handlers/gittitle.rb
@@ -2,6 +2,7 @@ module Lita
   module Handlers
     class Gittitle < Handler
 		  on :loaded, :set_route_issue
+		  on :loaded, :set_route_pull
       config(:web_endpoint,type: String, required: true)
       config :api_endpoint
       config :token
@@ -11,6 +12,10 @@ module Lita
         regexp = "(http[s]://" + config.web_endpoint + ")\/(.+)\/issues\/([0-9]+)"
         self.class.route(/#{regexp}/i,:issue,help:{"GHEのissueのURL" => "タイトルを返す"})
 			end
+			def set_route_pull(payload)
+        regexp = "(http[s]://" + config.web_endpoint + ")\/(.+)\/pull\/([0-9]+)"
+        self.class.route(/#{regexp}/i,:pull,help:{"GHEのPRのURL" => "タイトルを返す"})
+			end
 			def octokit()
 				Octokit.reset!
 				@@octokit = Octokit::Client.new(
@@ -18,6 +23,11 @@ module Lita
         :web_endpoint=> config.web_endpoint, 
         :api_endpoint =>config.api_endpoint)
 		  end 	
+      def pull(response)
+				octokit()
+        issue =  @@octokit.issue(response.matches[0][1],response.matches[0][2])
+	      response.reply(response.matches[0][0] + "/" + response.matches[0][1] + "/pull/" + response.matches[0][2] + "\n" + issue.title)
+      end
       def issue(response)
 				octokit()
         issue =  @@octokit.issue(response.matches[0][1],response.matches[0][2])
